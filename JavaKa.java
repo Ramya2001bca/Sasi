ba882720402ed9d80a30197420cd95c86559d74d
Ramya2001bca <113408822+Ramya2001bca@users.noreply.github.com>
2022-10-07 09:26:49 +0530

    Add files via upload

---
+++ JavaKa.java
@@ -0,0 +1,21 @@
+public class JavaKa{
+static String course="java";  //static -you can print the method
+int number=12;    //non satic value method create object and use that value
+
+
+static void notes(){
+String name="mathavi";
+System.out.println(name);
+System.out.println(course);
+}
+
+
+void book(){
+String name="ramya";
+System.out.println(name);
+System.out.println(course);
+//System.out.println(obj.number);
+}
+
+
+}
---
+++ Sasi.java
@@ -0,0 +1,11 @@
+public class Sasi{
+
+public static void main(String[] args){
+JavaKa obj=new JavaKa();
+obj.notes();
+obj.book();
+System.out.println(obj.course);
+System.out.println(obj.number);
+}
+
+}
